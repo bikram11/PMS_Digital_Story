777f57a981094d2f952a4c2e11d5ab2876592fde
Bikram Adhikari <bikramadhikari@Bikrams-MBP.fios-router.home>
2021-11-14 20:22:18 -0500

    testing out dynamic content

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 // Listening to a button click.
 $('[data-switch]').on('click', function (e) {
     var $page = $('#page-4'),
+    var $page_image = $('#page-4_image')
         blockToShow = e.currentTarget.getAttribute('data-switch');
 
     // Hide all children.
     $page.children().hide();
-
+    $page_image.children().hide();
     // And show the requested component.
     $page.children(blockToShow).show();
-    $page.children(blockToShow+"_image").show();
+    $page_image.children(blockToShow+"_image").show();
 });
