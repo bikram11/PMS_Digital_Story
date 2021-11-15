5580f59d4c69b1667f40b04069c9686eba520911
Bikram Adhikari <bikramadhikari@Bikrams-MBP.fios-router.home>
2021-11-14 20:43:40 -0500

    dynamic slide changes

--- script.js
+++ script.js
@@ -4,12 +4,15 @@
 $('[data-switch]').on('click', function (e) {
     var $page = $('#page-4'),
         blockToShow = e.currentTarget.getAttribute('data-switch'),
-        $page_image = $('#page-4_image');
+        $page_image = $('#page-4_image'),
+        $page_button = $('#page-4_buttons');
 
     // Hide all children.
     $page.children().hide();
     $page_image.children().hide();
+    $page_button.children().hide();
     // And show the requested component.
     $page.children(blockToShow).show();
     $page_image.children(blockToShow+"_image").show();
+    $page_button.children(blockToShow+"_buttons").show();
 });
