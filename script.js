7edb65091d1c60b239beeb841ffe58168fa210a7
Bikram Adhikari <bikramadhikari@Bikrams-MBP.fios-router.home>
2021-11-22 16:57:59 -0500

    image story added

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ $('[data-switch]').on('click', function (e) {
     $page_image.children(blockToShow+"_image").show();
     $page_button.children(blockToShow+"_buttons").show();
 });
+
+$('[data-switch]').on('click', function (e) {
+    var $page = $('#page-5'),
+        blockToShow = e.currentTarget.getAttribute('data-switch'),
+        $page_image = $('#page-5_image');
+
+    // Hide all children.
+    $page.children().hide();
+    $page_image.children().hide();
+    // And show the requested component.
+    $page.children(blockToShow).show();
+    $page_image.children(blockToShow+"_image").show();
+});
